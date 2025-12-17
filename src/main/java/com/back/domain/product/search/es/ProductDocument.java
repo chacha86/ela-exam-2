974e9a07c23bd00a334368d7dda8dbe9b7b5f116
chacha86 <taejincha0619@gmail.com>
2025-12-17 17:45:56 +0900

    analyzer setting, decompound_mode:mixed

--- src/main/java/com/back/domain/product/search/es/ProductDocument.java
+++ src/main/java/com/back/domain/product/search/es/ProductDocument.java
@@ -9,8 +9,12 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
 
 @Document(indexName = "products")
+@Setting(
+        settingPath = "elasticsearch/products-settings.json"
+)
 @Getter
 @Builder
 @AllArgsConstructor
@@ -20,7 +24,7 @@ public class ProductDocument {
     @Id
     private Long id;
 
-    @Field(type= FieldType.Text)
+    @Field(type= FieldType.Text, analyzer = "korean_index_analyzer")
     private String name;
 
     public static ProductDocument from(Product product) {
